1ffe3a0ab8a62472771d7dbc65d58d39a239e18c
amithnair91 <amithnair91@gmail.com>
2017-01-06 01:58:22 +0530

    limited progress percentage to 2 decimal places

--- src/main/java/com/amith/ui/ProgressUpdater.java
+++ src/main/java/com/amith/ui/ProgressUpdater.java
@@ -1,20 +1,29 @@
 package com.amith.ui;
 
+import java.math.BigDecimal;
+
 /**
  * Created by amithnair on 1/5/17.
  */
 public class ProgressUpdater {
 
-    private float percentageCompleted;
+    private float _percentageCompleted;
 
 
     public void displayProgress(long downloadedSize, long totalFileSize) {
         updateProgress(downloadedSize, totalFileSize);
-        System.out.println("Downloading file..... " + percentageCompleted + "%");
+        System.out.println("Downloading file..... " + _percentageCompleted + "%");
     }
 
     private void updateProgress(long downloadedSize, long totalFileSize) {
-        percentageCompleted = ((float)downloadedSize / (float)totalFileSize) * 100;
+        float percentage = ((float) downloadedSize / (float) totalFileSize) * 100;
+        _percentageCompleted = roundOff(percentage, 2);
+    }
+
+    private float roundOff(float percentage, int decimalPlace) {
+        BigDecimal decimal = new BigDecimal(Float.toString(percentage));
+        decimal = decimal.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return decimal.floatValue();
     }
 
 }
