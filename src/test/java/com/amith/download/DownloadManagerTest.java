6287594ce935da81e0ea9997ff86f20b82576123
amithnair91 <amithnair91@gmail.com>
2017-01-06 02:24:25 +0530

    adding temporary directory to download file for fixing failing test

--- src/test/java/com/amith/download/DownloadManagerTest.java
+++ src/test/java/com/amith/download/DownloadManagerTest.java
@@ -1,6 +1,5 @@
 package com.amith.download;
 
-import com.amith.connection.HttpConnection;
 import com.amith.model.HttpFile;
 import org.junit.After;
 import org.junit.Before;
@@ -8,8 +7,9 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -19,23 +19,32 @@ import static org.junit.Assert.assertTrue;
  */
 public class DownloadManagerTest {
 
-    private static final String FILE_DESTINATION = "http.svg";
+
+    private static final String TEMP_DIR_NAME = "temp-download";
     private static final String DOWNLOAD_URL = "http://httpwg.org/asset/http.svg";
+    private String fileName = "http.svg";
+
 
     private HttpFile httpFile;
     private File downloadedFile;
+    private String FILE_DESTINATION;
+    private Path tempDirPath;
 
     @Before
     public void setup() throws IOException {
+        tempDirPath = Files.createTempDirectory(TEMP_DIR_NAME);
+        FILE_DESTINATION =  tempDirPath.toAbsolutePath().toString().concat(fileName);
         httpFile = new HttpFile(new URL(DOWNLOAD_URL), FILE_DESTINATION);
     }
 
     @Test
     public void shouldBeAbleToDownloadFile() throws IOException, InterruptedException {
         DownloadManager downloadManager = new DownloadManager(httpFile);
+        
         downloadManager.startDownload();
         Thread.sleep(3000);
         downloadedFile = new File(FILE_DESTINATION);
+        
         assertTrue(new File(FILE_DESTINATION).exists());
         assertEquals(2771, httpFile.getDownloadedFileSize());
     }
@@ -59,6 +68,7 @@ public class DownloadManagerTest {
     @After
     public void tearDown() {
         downloadedFile.delete();
+        tempDirPath.toFile().delete();
     }
 
 
